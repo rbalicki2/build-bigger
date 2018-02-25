2379c17ae773a3d8139a84ac9bcd0b3844777f75
Robert <robert.balicki@gmail.com>
2018-02-24 21:58:17 -0500

    include babel polyfill

--- src/components/App/AutocompletePage/index.jsx
+++ src/components/App/AutocompletePage/index.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 
-export default () => <div>autocomplete</div>;
+export default () => (<div style={{ maxWidth: 800, margin: '50px auto' }}>
+  <div style={{ textAlign: 'center' }}>
+    <h1>Autocomplete Stage 0</h1>
+    <h2>One long file</h2>
+    <h3>Tag: v0</h3>
+  </div>
+  
+</div>);
--- src/components/App/HomePage.jsx
+++ src/components/App/HomePage.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => (<div style={{ maxWidth: 800, margin: '50px auto' }}>
-  <h1 style={{ textAlign: 'center' }}>Building Bigger By Building Smaller</h1>
+  <div style={{ textAlign: 'center' }}>
+    <h1>Building Bigger By Building Smaller</h1>
+    <h2>Robert Balicki</h2>
+  </div>
   <div>
     You can:
     <ul>
       <li><a href="/presentation">View the presentation</a></li>
       <li><a href="https://github.com/rbalicki2/build-bigger">View the repository</a></li>
       <li><Link to="/autocomplete">View the autocomplete widget</Link></li>
+      <li><a href="https://www.robertbalicki.com">Visit my website</a></li>
     </ul>
   </div>
 </div>);
